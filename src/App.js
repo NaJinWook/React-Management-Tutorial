c1c78da8632731677f23a2264ffe6c8d50c6f723
나진욱 <jwna@daumsoft.com>
2020-07-09 17:52:28 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,51 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { Component } from "react";
+import Customer from "./components/Customer";
 import "./App.css";
+
+const customers = [
+  {
+    id: "1",
+    image: "https://placeimg.com/64/64/1",
+    name: "나진욱",
+    birthday: "940720",
+    gender: "남자",
+    job: "직장인",
+  },
+  {
+    id: "2",
+    image: "https://placeimg.com/64/64/2",
+    name: "손예진",
+    birthday: "880720",
+    gender: "여자",
+    job: "배우",
+  },
+  {
+    id: "3",
+    image: "https://placeimg.com/64/64/3",
+    name: "원빈",
+    birthday: "770720",
+    gender: "남자",
+    job: "존잘러",
+  },
+];
 
 class App extends React.Component {
   render() {
     return (
-      <div className="gray-background">
-        <img src={logo} alt="logo" />
-        <h2>Let's develop management system!</h2>
+      <div>
+        {customers.map((c) => {
+          return (
+            <Customer
+              key={c.id}
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.gender}
+              job={c.job}
+            />
+          );
+        })}
       </div>
     );
   }
